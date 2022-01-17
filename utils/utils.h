2f13f7f08803074cb309ef37d4f47a67817b1553
Aki <msf5pbsi@gmail.com>
2022-01-17 12:19:04 +0900

    updated util.h

--- utils/utils.h
+++ utils/utils.h
@@ -1,1 +1,9 @@
+#ifndef ALGORITHM_UTILS
+#define ALGORITHM_UTILS
 #include<iostream>
+#include<unordered_map>
+namespace algorithm::util{
+    template<typename T>
+    bool validate_sorted_array(T arr);
+}
+#endif
