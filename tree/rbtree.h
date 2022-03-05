eba3e1fcac080f58cd60beda569a82f44227179b
Aki T <84285825+akt00@users.noreply.github.com>
2022-03-05 13:00:00 +0900

    Update rbtree.h

--- tree/rbtree.h
+++ tree/rbtree.h
@@ -1,7 +1,7 @@
 #ifndef ALGORITHM_RED_BLACK_TREE
 #define ALGORITHM_RED_BLACK_TREE
-
 #include<algorithm>
+
 
 // 1. root is black
 // 2. nil is black
@@ -32,24 +32,22 @@ namespace algorithm::rbtree {
 		RedBlackTree(const RedBlackTree&) = delete;
 		RedBlackTree& operator=(const RedBlackTree&) = delete;
 		~RedBlackTree();
-		
 		node<K, V>* find_node(K key) const; // returns a pointer to the node or nullptr(key not found)
 		void rb_insert(K key, V value);
 		void rb_delete(K key);
-		void rb_delete_all_nodes();
+		void rb_delete_all_nodes(); // deallocates all nodes
 		void show_tree() const; // displays the tree info
-
+		
 	private:
 		node<K, V>* find_node(K key, node<K, V>* current) const; // helper function
 		void left_rotation(node<K, V>* x); // rotate the subtree around current to the left
 		void right_rotation(node<K, V>* y); // rotate to the right
 		void rb_insert_self_balance(node<K, V>* current); // perform self balancing
-		auto rb_min(node<K, V>* current) const -> decltype(this->root); // find the minimum node from the subtree of current
+		node<K, V>* rb_min(node<K, V>* current) const; // find the minimum node from the subtree of current
 		void rb_transplant(node<K, V>* x, node<K, V>* y); // replace x with y
 		void rb_delete_self_balance(node<K, V>* current); // perform self balancing
-		void rb_delete_all_nodes(node<K,V>* current);
+		void rb_delete_all_nodes(node<K,V>* current); // helper functoin
 		void show_tree(node<K, V>* current, int node_num = 0) const; // helper function. root = 0
-		
 	};
 }
 
