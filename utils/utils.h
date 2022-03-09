c967d815b212662f036bc87a69211151154b7e85
Aki T <84285825+akt00@users.noreply.github.com>
2022-03-09 21:49:30 +0900

    Update utils.h

--- utils/utils.h
+++ utils/utils.h
@@ -1,15 +1,23 @@
 #ifndef ALGORITHM_UTILS
 #define ALGORITHM_UTILS
+
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<algorithm>
 #include<unordered_map>
 
-
-namespace algorithm::util {
+namespace algorithm::utils {
     
         // arr should be either std::array or std::vector
         template<typename T>
         bool validate_sorted_array(T arr);
     }
+
+    // load iris dataset 
+    namespace iris {
+        using data = std::pair<std::vector<std::vector<float>>, std::vector<int>>;
 }
 
 #endif
