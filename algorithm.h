a2aab4acdbace0d926bda4e9014925547dc63be0
Aki T <84285825+akt00@users.noreply.github.com>
2022-04-09 18:03:11 +0900

    Update algorithm.h

--- algorithm.h
+++ algorithm.h
@@ -2,6 +2,17 @@
 #define ALGORITHM
 // top level header
 
+/* naming conventions: naming conventions are mostly in compliance with https://google.github.io/styleguide/cppguide.html
+*  variable: all lowercase with words separated with "_"
+*  struct/class: all lowercase or CamelCase in case of multiple words
+*  function/method: camelCase
+*  enumerator: CamelCase
+*  template/concept typename: one capital letter or CamelCase
+*  namespace: all lowercase with words separated with "_"
+*  macro: all uppercase with words separated with "_"
+*  file name: all lowercase with words separated with "_"
+*/
+
 #include"container/container.h"
 
 #include"dp/dp.h"
