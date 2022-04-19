b2ca0a0a78995e1fb05a56cd1199cbccd09fbeef
Aki T <84285825+akt00@users.noreply.github.com>
2022-04-19 17:53:30 +0900

    Update sort.h

--- sort/sort.h
+++ sort/sort.h
@@ -1,1 +1,24 @@
+#ifndef ALGORITHM_SORT
+#define ALGORITHM_SORT
 
+
+#include"bucketSort.h"
+
+#include"countingSort.h"
+
+#include"insertionSort.h"
+
+#include"introSort.h"  // c++ stl sorting algorithm
+
+#include"heapSort.h"
+
+#include"mergeSort.h"
+
+#include"quickSort.h"
+
+#include"radixSort.h"
+
+#include"timSort.h" // python stl sorting algorithm
+
+
+#endif // ALGORITHM_SORT
