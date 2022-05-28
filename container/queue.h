cf943d985834c4818f741d52133d037bec98ebec
Aki T <84285825+akt00@users.noreply.github.com>
2022-05-28 19:10:41 +0900

    Update queue.h

--- container/queue.h
+++ container/queue.h
@@ -20,74 +20,77 @@ namespace algorithm::container {
 		unsigned queue_size;
 	public:
 		queue();
-		queue(const queue<T>& arg);
-		queue& operator=(const queue<T>& arg);
+		queue(const queue<T>& other);
+		queue& operator=(const queue<T>& other);
 		~queue();
-		const QueueStackNode<T>& front() const;
-		const QueueStackNode<T>& back() const;
+		const T& front() const; // out, must call empty() in advance
+		const T& back() const; // in, must call empty() in advance
 		bool empty() const; // calling top() while empty() causes an error
 		unsigned size() const;
-		void push(T data);
+		void push(T data); // this implementation call copy consturctor twice on user objects
 		void pop();
 		void clear();
+		void swap(queue<T>& other);
 	};
+
+
+	// implementations
 
 	template<typename T>
 	queue<T>::queue() {
+
 		head = nullptr;
 		tail = nullptr;
 		queue_size = 0;
 	}
 
 	template<typename T>
-	queue<T>::queue(const queue<T>& arg) {
-		clear();
-		auto current = arg.head;
+	queue<T>::queue(const queue<T>& other) {
+
+		head = nullptr;
+		tail = nullptr;
+		auto current = other.head;
 		while (current != nullptr) {
 			push(current->data);
 			current = current->next;
 		}
+		queue_size = other.queue_size;
 	}
 
 	template<typename T>
-	queue<T>& queue<T>::operator=(const queue<T>& arg) {
-		if (&arg != this) {
+	queue<T>& queue<T>::operator=(const queue<T>& other) {
+
+		if (&other != this) {
 			clear();
-			auto current = arg.head;
+			head = nullptr;
+			tail = nullptr;
+			auto current = other.head;
 			while (current != nullptr) {
 				push(current->data);
 				current = current->next;
 			}
+			queue_size = other.queue_size;
 		}
 	}
 
 	template<typename T>
-	queue<T>::~queue() {
-		clear();
-	}
+	queue<T>::~queue() { clear(); }
 
 	template<typename T>
-	const QueueStackNode<T>& queue<T>::front() const {
-		return *head;
-	}
+	const T& queue<T>::front() const { return head->data; }
 
 	template<typename T>
-	const QueueStackNode<T>& queue<T>::back() const {
-		return *tail;
-	}
+	const T& queue<T>::back() const { return tail->data; }
 
 	template<typename T>
-	bool queue<T>::empty() const {
-		return (queue_size > 0) ? true : false;
-	}
+	bool queue<T>::empty() const { return (queue_size == 0) ? true : false; }
 
 	template<typename T>
-	unsigned queue<T>::size() const {
-		return queue_size;
-	}
+	unsigned queue<T>::size() const { return queue_size; }
 
 	template<typename T>
 	void queue<T>::push(T data) {
+
 		auto temp = new QueueStackNode<T>{ data, nullptr };
 		if (tail == nullptr) {
 			head = temp;
@@ -102,7 +105,8 @@ namespace algorithm::container {
 
 	template<typename T>
 	void queue<T>::pop() {
-		if (empty() == false) {
+
+		if (!empty()) {
 			auto temp = head;
 			if (head == tail) { // avoid dangling pointer
 				head = nullptr;
@@ -117,9 +121,22 @@ namespace algorithm::container {
 	}
 
 	template<typename T>
-	void queue<T>::clear() {
-		while (empty() == false) {
-			pop();
+	void queue<T>::clear() { while (!empty()) pop(); }
+
+	template<typename T>
+	void queue<T>::swap(queue<T>& other) {
+
+		if (&other != this) {
+			auto tmp_head = other.head;
+			auto tmp_tail = other.tail;
+			other.head = head;
+			other.tail = tail;
+			head = tmp_head;
+			tail = tmp_tail;
+
+			auto tmp_size = other.queue_size;
+			other.queue_size = queue_size;
+			queue_size = tmp_size;
 		}
 	}
 }
